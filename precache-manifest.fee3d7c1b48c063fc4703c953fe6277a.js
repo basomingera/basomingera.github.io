eb9a52c28e44be2a978f93e46ac019376b225fee
Rob <basorot@gmail.com>
2020-09-28 15:28:55 +0900

    Updates

---
+++ precache-manifest.fee3d7c1b48c063fc4703c953fe6277a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f6367b03c39fb7d30928bd7f5132e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09a391bbd19c119f02e6",
+    "url": "/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "dfcb71a6ac99f038c604",
+    "url": "/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "09a391bbd19c119f02e6",
+    "url": "/static/js/2.eb667dda.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.eb667dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfcb71a6ac99f038c604",
+    "url": "/static/js/main.18ef6d32.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  }
+]);
